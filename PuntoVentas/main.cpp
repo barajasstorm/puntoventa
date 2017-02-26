fb5482f062a0b2d2b17024008d37ae68ec2f54f1
barajasstorm <jbarajas@servsuite.net>
2017-02-26 01:19:21 -0600

    Initial Commit

---
+++ PuntoVentas/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PuntoVentas
+//
+//  Created by Juan Barajas on 2/26/17.
+//  Copyright © 2017 Juan Barajas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
